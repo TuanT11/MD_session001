43005049a04f4dd45f0825ddefc73d5a7a6fa82d
Nguyen Trung Tuan <pesgametuan@gmail.com>
2026-04-20 20:24:41 +0700

    Bai2

---
+++ Bai2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Bai2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập số thứ nhất (firstNumber) :");
+        int firstNumber = sc.nextInt();
+        System.out.print("Nhập số thứ hai (secondNumber) : ");
+        int secondNumber = sc.nextInt();
+
+        System.out.println("--- Kết quả ---");
+        System.out.println("firstNumber = " + firstNumber);
+        System.out.println("secondNumber = " + secondNumber);
+        System.out.println("Tổng : " + (firstNumber + secondNumber));
+        System.out.println("Hiệu : " + (firstNumber - secondNumber));
+        System.out.println("Tích : " + (firstNumber * secondNumber));
+        System.out.println("Thương : " + (firstNumber / secondNumber));
+        System.out.println("Phần dư : " + (firstNumber % secondNumber));
+    }
+}
