fe1607bf432ac205e297eb7a02d942757041feb3
Nguyen Trung Tuan <pesgametuan@gmail.com>
2026-04-20 20:50:42 +0700

    Bai5

---
+++ Bai5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Bai5{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Cân nặng : ");
+        float weight = sc.nextFloat();
+        System.out.print("Chiều cao : ");
+        float height = sc.nextFloat();
+        System.out.printf("Chỉ số BMI: %.2f", (long)weight/Math.pow(height, 2));
+    }
+}
